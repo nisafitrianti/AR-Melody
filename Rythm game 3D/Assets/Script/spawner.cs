c1640b1f98ec4ebed57ff5083e84c67ead54c922
nisafitrianti <you@example.com>
2021-02-16 12:47:06 +0700

    Piano Hero

---
+++ Rythm game 3D/Assets/Script/spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject[] nots;
+    public Transform[] points;
+    public float beat = (60/30) * 2;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > beat) {
+            GameObject not = Instantiate(nots[Random.Range(0, 14)], points[Random.Range(0, 4)]);
+            not.transform.localPosition = Vector3.zero;
+            timer -= beat;
+        }
+
+        timer += Time.deltaTime; 
+    }
+}
